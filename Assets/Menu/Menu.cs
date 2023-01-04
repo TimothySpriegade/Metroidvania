2d0602581e643d9302b6193f79f97a1dfe3d58fc
Timothy Spriegade <timothyspriegade@outlook.de>
2023-01-04 20:53:11 +0100

    big menu commit - made working menu (MainMenu), Quit button works, option button works, start button works

--- Assets/Menu/Menu.cs
+++ Assets/Menu/Menu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
@@ -14,5 +15,16 @@ public class Menu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PlayGame()
+    {
+        //lädt nächste Szene in Scenemanager - änder sich vllt noch wegen save
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
     }
 }
