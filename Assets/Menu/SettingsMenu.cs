2006d0b72f55d1969e51d7dd1cf2ae4d5d88bc1b
Timothy Spriegade <timothyspriegade@outlook.de>
2023-01-04 21:26:32 +0100

    added automatic resolution check

--- Assets/Menu/SettingsMenu.cs
+++ Assets/Menu/SettingsMenu.cs
@@ -15,12 +15,20 @@ public class SettingsMenu : MonoBehaviour
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
         for (int i = 0; i <resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
-            options.Add(option); 
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
     public void SetSFXVolume(float volume)
     {
