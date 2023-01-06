3051dd3442287ea1954cecf755b56d1706c33100
Timothy Spriegade <timothyspriegade@outlook.de>
2023-01-06 18:59:25 +0100

    added working sound slider and mixer tracks

--- Assets/Menu/SettingsMenu.cs
+++ Assets/Menu/SettingsMenu.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
     //vars
     Resolution[] resolutions;
     public TMPro.TMP_Dropdown resolutionDropdown;
+
+    public AudioMixer mixer;
+    public Slider masterSlider, musicSlider, effectsSlider;
+
 
     private void Start()
     {
@@ -35,14 +40,26 @@ public class SettingsMenu : MonoBehaviour
         Resolution resoltion = resolutions[resolutionIndex];
         Screen.SetResolution(resolutions[resolutionIndex].width, resolutions[resolutionIndex].height, Screen.fullScreen);
     }
+
+    public void SetMasterVolume(float volume)
+    {
+        Debug.Log(volume);
+        mixer.SetFloat("MasterVolume", masterSlider.value);
+        PlayerPrefs.SetFloat("MasterVolume", masterSlider.value);
+    }
     public void SetSFXVolume(float volume)
     {
         Debug.Log(volume);
+        mixer.SetFloat("EffectVolume", effectsSlider.value);
+        PlayerPrefs.SetFloat("EffectVolume", effectsSlider.value);
     }
     public void SetMusicVolume(float volume)
     {
         Debug.Log(volume);
+        mixer.SetFloat("MusicVolume", musicSlider.value);
+        PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
     }
+
     public void SetFullScreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
