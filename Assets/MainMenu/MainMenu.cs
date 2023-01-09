da49075e7d315366d327d82dcd9493ec8920bfca
Niclas Kürschner <niclas.kuerschner@nordakademie.de>
2023-01-09 21:24:50 +0100

    Changed Resolution to be a distinct list to ignore refresh rates

--- Assets/MainMenu/MainMenu.cs
+++ Assets/MainMenu/MainMenu.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+namespace MainMenu
 {
+    public class MainMenu : MonoBehaviour
+    {
    
-    public void PlayGame()
-    {
-        //lädt nächste Szene in Scenemanager - änder sich vllt noch wegen save
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-    public void QuitGame()
-    {
-        Debug.Log("Quit");
-        Application.Quit();
+        public void PlayGame()
+        {
+            //lädt nächste Szene in Scenemanager - änder sich vllt noch wegen save
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        public void QuitGame()
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
     }
 }
--- Assets/MainMenu/SettingsMenu.cs
+++ Assets/MainMenu/SettingsMenu.cs
@@ -1,67 +1,84 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Audio;
 
-public class SettingsMenu : MonoBehaviour
+namespace MainMenu
 {
-    //vars
-    private Resolution[] resolutions;
-    [SerializeField] private TMPro.TMP_Dropdown resolutionDropdown;
-    [SerializeField] private AudioMixer mixer;
-    [SerializeField] Slider masterSlider, musicSlider, effectsSlider;
-    [SerializeField] private AudioManager manager;
-    
-
-
-    private void Start()
+    public class SettingsMenu : MonoBehaviour
     {
-        //clears DropDown and adds all possible resolutions
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-        var options = new List<string>();
-        var currentResolutionIndex = 0;
-        foreach (var resolution in resolutions)
+        // Components
+        [SerializeField] private TMPro.TMP_Dropdown resolutionDropdown;
+        [SerializeField] private Slider masterSlider, musicSlider, effectsSlider;
+        [SerializeField] private AudioManager audioManager;
+        [SerializeField] private Toggle fullscreenToggle;
+
+        // Variables
+        private List<Resolution> addedResolutions;
+
+
+        private void Start()
         {
-            var option = resolution.width + "x" + resolution.height;
-            options.Add(option);
-            if (resolution.width == Screen.currentResolution.width &&
-                resolution.height == Screen.currentResolution.height)
+            //clears DropDown and adds all possible resolutions
+            var resolutions = Screen.resolutions;
+            addedResolutions = new List<Resolution>();
+            var options = new List<string>();
+
+            var currentResolutionIndex = 0;
+
+            resolutionDropdown.ClearOptions();
+
+            foreach (var resolution in resolutions)
             {
-                currentResolutionIndex = options.IndexOf(option);
+                var option = resolution.width + "x" + resolution.height;
+                if (!options.Contains(option))
+                {
+                    options.Add(option);
+                    addedResolutions.Add(resolution);
+                    if (resolution.width == Screen.currentResolution.width &&
+                        resolution.height == Screen.currentResolution.height)
+                    {
+                        currentResolutionIndex = options.IndexOf(option);
+                    }
+                }
             }
+
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
+
+            // Initialize volume sliders
+            masterSlider.value = audioManager.MasterVolume;
+            musicSlider.value = audioManager.MusicVolume;
+            effectsSlider.value = audioManager.EffectVolume;
+
+            // Initialize fullScreen toggle
+            fullscreenToggle.SetIsOnWithoutNotify(Screen.fullScreen);
         }
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
 
-    
-        masterSlider.value = manager.MasterVolume;
-        musicSlider.value = manager.MusicVolume;
-        effectsSlider.value = manager.EffectVolume;
-    }
-  
-    public void SetResolution(int resolutionIndex)
-    {
-        Screen.SetResolution(resolutions[resolutionIndex].width, resolutions[resolutionIndex].height, Screen.fullScreen);
-    }
+        public void SetResolution(int resolutionIndex)
+        {
+            var resolution = addedResolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        }
 
-    public void SetMasterVolume(float volume)
-    {
-        manager.MasterVolume = volume;
-    }
-    public void SetSFXVolume(float volume)
-    {
-        manager.EffectVolume = volume;
-    }
-    public void SetMusicVolume(float volume)
-    {
-        manager.MusicVolume = volume;
-    }
+        public void SetMasterVolume(float volume)
+        {
+            audioManager.MasterVolume = volume;
+        }
 
-    public void ToggleFullScreen()
-    {
-        Screen.fullScreen = !Screen.fullScreen;
+        public void SetSFXVolume(float volume)
+        {
+            audioManager.EffectVolume = volume;
+        }
+
+        public void SetMusicVolume(float volume)
+        {
+            audioManager.MusicVolume = volume;
+        }
+
+        public void ToggleFullScreen()
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+        }
     }
 }
