6cea2a76496d473990198ddbb494449f4441975b
Niclas Kürschner <niclas.kuerschner@nordakademie.de>
2023-01-10 11:47:38 +0100

    Added Cinemachine Package, rewrote PlayerCamera script to fit Cinemachine to remove direct dependency to Player prefab

--- Assets/Player/PlayerCamera/PlayerCamera.cs
+++ Assets/Player/PlayerCamera/PlayerCamera.cs
@@ -1,23 +1,26 @@
+using System;
 using UnityEngine;
+using Cinemachine;
 
 namespace Player.PlayerCamera
 {
     public class PlayerCamera : MonoBehaviour
     {
-        private Vector3 playerPos;
-        [SerializeField] private GameObject player;
-
-
-        private void FollowPlayer()
+        private CinemachineVirtualCamera playerCamera;
+        private GameObject player;
+        private void Start()
         {
-            playerPos = player.transform.position;
-            playerPos = Vector3.Lerp(transform.position, playerPos, 10f * Time.deltaTime);
-            transform.position = new Vector3(playerPos.x, playerPos.y, transform.position.z);
+            playerCamera = GetComponent<CinemachineVirtualCamera>();
+            player = GameObject.FindGameObjectWithTag("Player");
         }
 
-        private void LateUpdate()
+        private void Update()
         {
-            FollowPlayer();
+            if (!ReferenceEquals(player, null))
+            {
+                playerCamera.Follow = player.transform;
+            }
         }
     }
+
 }
