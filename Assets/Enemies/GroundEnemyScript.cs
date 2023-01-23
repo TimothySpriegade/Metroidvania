9baa51662b69b7c719263103c6aec8f55bc97d9f
Timothy Spriegade <timothyspriegade@outlook.de>
2023-01-23 08:30:34 +0100

    added chasing check and chasing

--- Assets/Enemies/GroundEnemyScript.cs
+++ Assets/Enemies/GroundEnemyScript.cs
@@ -23,15 +23,42 @@ public class GroundEnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        getDistToPlayer();
-       
+        GetDistToPlayer();
+        ChasePlayerAI();
     }
     #endregion
 
-    #region DistanceCheck
-    public void getDistToPlayer()
+    #region DistanceCheck&Chasing
+    private void GetDistToPlayer()
     {
         distanceToPlayer = Vector2.Distance(transform.position, player.position);
     }
+
+    private void ChasePlayerAI()
+    {
+        if (distanceToPlayer < aggroRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            //stop chasing
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        if(transform.position.x < player.position.x)
+        {
+            // Gegner  auf der linken Seite des Spieler, laufe rechts
+            rb.velocity = new Vector2(moveSpeed, 0);
+        }
+        else
+        {
+            // Gegner  auf der rechten Seite des Spieler, laufe links
+            rb.velocity = new Vector2(-moveSpeed, 0);
+        }
+    }
+
     #endregion
 }
