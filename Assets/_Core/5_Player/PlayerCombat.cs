5b3df2aa0e184c57a6b5e63bd10ddd9beff2fd40
Timothy Spriegade <timothyspriegade@outlook.de>
2023-01-30 17:10:08 +0100

    added attack hit

--- Assets/_Core/5_Player/PlayerCombat.cs
+++ Assets/_Core/5_Player/PlayerCombat.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCombat : MonoBehaviour
 {
     #region vars
+
     #region playercombatvars
 
     [Header("Combat")]
@@ -14,5 +16,41 @@ public class PlayerCombat : MonoBehaviour
     [SerializeField] private LayerMask enemyLayer;
 
     #endregion
+
+    #region Input
+
+    [Header("Input")]
+    [SerializeField] private InputActionReference attackInput;
+
     #endregion
+
+    #endregion
+
+    #region UnityMethods
+
+    public void Update()
+    {
+       if (attackInput != null) 
+        {
+            Attack();
+        }
+    }
+
+    #endregion
+
+    void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach(Collider2D enemy in hitEnemies) 
+        {
+            Debug.Log("hit" + enemy.name);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
 }
