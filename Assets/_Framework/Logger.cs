76102e46ee22c616dfe7d3c51885cc81162a9e7d
Niclas Kürschner <niclas.kuerschner@nordakademie.de>
2023-02-12 21:33:09 +0100

    Added Logger

---
+++ Assets/_Framework/Logger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace _Framework
+{
+    public static class Logger
+    {
+        public static void Log(this Object loggingObject, object message)
+        {
+#if UNITY_EDITOR
+            var className = loggingObject.name;
+            message = $"{className} {message}";
+            Debug.Log(message);
+#endif
+        }
+    }
+}
