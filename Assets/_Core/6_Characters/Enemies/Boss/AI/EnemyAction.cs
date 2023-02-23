22c558d87451722d3656d3587be46304ec61f20d
Niclas Kürschner <niclas.kuerschner@nordakademie.de>
2023-02-23 21:58:26 +0100

    WIP

---
+++ Assets/_Core/6_Characters/Enemies/Boss/AI/EnemyAction.cs
@@ -0,0 +1,19 @@
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+namespace _Core._6_Characters.Enemies.Boss.AI
+{
+    public class EnemyAction : Action
+    {
+        protected Rigidbody2D rb;
+        protected Animator animator;
+        protected Destructible destructible;
+
+        public override void OnAwake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            destructible = GetComponent<Destructible>();
+            animator = gameObject.GetComponentInChildren<Animator>();
+        }
+    }
+}
