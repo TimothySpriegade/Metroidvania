e0794ee9edef09c9f713fa6a7b6be03a2db23430
Niclas Kürschner <niclas.kuerschner@nordakademie.de>
2023-03-05 12:39:26 +0100

    moved player reference to BossCombat to improve performance

---
+++ Assets/_Core/6_Characters/Enemies/Boss/BossCombat.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace _Core._6_Characters.Enemies.Boss
+{
+    public class BossCombat : Destructible
+    {
+        public GameObject player { get; private set; }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+}
