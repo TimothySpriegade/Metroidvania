ea341cdcfdfc60b0932b06bdfdb5f4d67707dc4a
Timothy Spriegade <timothyspriegade@outlook.de>
2023-03-08 17:06:47 +0100

    fixed GetContact thing

--- Assets/_Core/6_Characters/Enemies/PassiveEnemy/PassiveEnemy.cs
+++ Assets/_Core/6_Characters/Enemies/PassiveEnemy/PassiveEnemy.cs
@@ -15,8 +15,8 @@ namespace _Core._6_Characters.Enemies.PassiveEnemy
         [SerializeField] private float accelRate;
 
         private float moveSpeed;
+        private bool isOnTopOfEnemy;
 
-        private bool collisionFromTop;
 
         #endregion
 
@@ -46,15 +46,15 @@ namespace _Core._6_Characters.Enemies.PassiveEnemy
 
         private void Update()
         {
-            if (rb.velocity.x != 0)
+            if (rb.velocity.x != 0 && !isOnTopOfEnemy)
             {
                 CheckDirectionToFace(rb.velocity.x > 0);
             }
-            
             //col.GetContact(0).normal.y > 0.5f; 
             // ^ Gibt true zurück, wenn der Kontaktpunk von oben kommt
             // Kann dir gerne aufzeichnen wie der normal vector funktioniert
-            
+
+
             if (!duringAnimation)
             {
                 EnemyAI();
@@ -78,6 +78,8 @@ namespace _Core._6_Characters.Enemies.PassiveEnemy
                     index = 0;
                 }
             }
+
+           
 
             var difference = idlePoints[index].position.x - transform.position.x;
             var targetSpeed = Mathf.Sign(difference) * accelRate;
@@ -106,14 +108,17 @@ namespace _Core._6_Characters.Enemies.PassiveEnemy
 
         private void OnCollisionEnter2D(Collision2D col)
         {
-            collisionFromTop = col.GetContact(0).normal.y > 0.5f;
-            this.Log(collisionFromTop);
-            if (col.gameObject.CompareTag("Player") && !collisionFromTop)
+            isOnTopOfEnemy = col.GetContact(0).normal.y < -0.5f && col.gameObject.CompareTag("Player") ? true : false;
+            if (col.gameObject.CompareTag("Player") && !(col.GetContact(0).normal.y < -0.5f))
             {
                 col.gameObject.GetComponent<PlayerCombat>().OnAttackHit(enemyData.damage);
             }
-          
         }
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            isOnTopOfEnemy = false;
+        }
+
 
     }
     public enum PassiveEnemyAnimatorState
