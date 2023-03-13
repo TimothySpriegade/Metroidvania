50dce8456de2a4003d54d3db48061f17c42093bb
Niclas Kürschner <niclas.kuerschner@nordakademie.de>
2023-03-13 20:59:26 +0100

    added Hazard for boss attack hitbox

---
+++ Assets/_Core/8_Environment/Traps/Hazard.cs
@@ -0,0 +1,18 @@
+﻿using _Core._5_Player;
+using UnityEngine;
+
+namespace _Core._8_Environment.Traps
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class Hazard : MonoBehaviour
+    {
+        [SerializeField] private int damage;
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.gameObject.CompareTag("Player"))
+            {
+                col.gameObject.GetComponent<PlayerCombat>().OnAttackHit(damage, gameObject);
+            }
+        }
+    }
+}
