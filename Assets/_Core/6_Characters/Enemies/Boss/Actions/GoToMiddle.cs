b9af39bef125defaddb2782e2e1f70965f195d88
Niclas Kürschner <niclas.kuerschner@nordakademie.de>
2023-03-14 15:44:40 +0100

    added GoToMiddle and LookToMiddle Action

---
+++ Assets/_Core/6_Characters/Enemies/Boss/Actions/GoToMiddle.cs
@@ -0,0 +1,29 @@
+﻿using _Core._6_Characters.Enemies.Boss.AI;
+using BehaviorDesigner.Runtime.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace _Core._6_Characters.Enemies.Boss.Actions
+{
+    public class GoToMiddle : EnemyAction
+    {
+        [SerializeField] private float walkDuration = 1;
+        private Tween walkTween;
+        
+        public override void OnStart()
+        {
+            bossEnemy.ChangeAnimationState(BossAnimatorState.BossRun);
+            walkTween = transform.DOMoveX(0, walkDuration).SetEase(Ease.Linear);
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            return walkTween.active ? TaskStatus.Running : TaskStatus.Success;
+        }
+
+        public override void OnEnd()
+        {
+            walkTween?.Kill();
+        }
+    }
+}
---
+++ Assets/_Core/6_Characters/Enemies/Boss/Actions/LookToMiddle.cs
@@ -0,0 +1,24 @@
+﻿using _Core._6_Characters.Enemies.Boss.AI;
+using BehaviorDesigner.Runtime.Tasks;
+
+namespace _Core._6_Characters.Enemies.Boss.Actions
+{
+    public class LookToMiddle : EnemyAction
+    {
+        private bool hasTurned;
+
+        public override void OnStart()
+        {
+            var targetIsToRight = transform.position.x < 0;
+            
+            bossEnemy.CheckDirectionToFace(targetIsToRight);
+
+            hasTurned = true;
+        }
+        
+        public override TaskStatus OnUpdate()
+        {
+            return hasTurned ? TaskStatus.Success : TaskStatus.Running;
+        }
+    }
+}
