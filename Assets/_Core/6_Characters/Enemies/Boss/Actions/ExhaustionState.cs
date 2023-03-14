aa057e6e29142624dd7748afed314a107dc894a6
Timothy Spriegade <timothyspriegade@outlook.de>
2023-03-14 16:30:45 +0100

    added animationstate

--- Assets/_Core/6_Characters/Enemies/Boss/Actions/ExhaustionState.cs
+++ Assets/_Core/6_Characters/Enemies/Boss/Actions/ExhaustionState.cs
@@ -1,3 +1,4 @@
+using _Core._6_Characters.Enemies.Boss;
 using _Core._6_Characters.Enemies.Boss.AI;
 using BehaviorDesigner.Runtime.Tasks;
 using System.Collections;
@@ -10,6 +11,10 @@ public class ExhaustionState : EnemyAction
     [SerializeField] private float exhaustionLength = 10;
     private float exhaustionTimer;
 
+    public override void OnStart()
+    {
+        bossEnemy.ChangeAnimationState(BossAnimatorState.BossExhausted);
+    }
     public override TaskStatus OnUpdate()
     {
         exhaustionTimer = Time.time;
