66175ddb734c9009cd0b13d0df8a72fdab170109
Niclas Kürschner <niclas.kuerschner@nordakademie.de>
2023-03-15 17:49:20 +0100

    added namespace to ExhaustionState

--- Assets/_Core/6_Characters/Enemies/Boss/Actions/ExhaustionState.cs
+++ Assets/_Core/6_Characters/Enemies/Boss/Actions/ExhaustionState.cs
@@ -1,28 +1,27 @@
-using _Core._6_Characters.Enemies.Boss;
 using _Core._6_Characters.Enemies.Boss.AI;
 using BehaviorDesigner.Runtime.Tasks;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class ExhaustionState : EnemyAction
+namespace _Core._6_Characters.Enemies.Boss.Actions
 {
-    [SerializeField] private float exhaustionLength = 10;
-    private float exhaustionTimer;
+    public class ExhaustionState : EnemyAction
+    {
+        [SerializeField] private float exhaustionLength = 10;
+        private float exhaustionTimer;
 
-    public override void OnStart()
-    {
-        bossEnemy.ChangeAnimationState(BossAnimatorState.BossExhausted);
-    }
-    public override TaskStatus OnUpdate()
-    {
-        exhaustionTimer = Time.time;
-        if(exhaustionTimer > exhaustionLength) 
+        public override void OnStart()
         {
-            exhaustionTimer = 0;
-            return TaskStatus.Success;
+            bossEnemy.ChangeAnimationState(BossAnimatorState.BossExhausted);
         }
-        return TaskStatus.Running;
+        public override TaskStatus OnUpdate()
+        {
+            exhaustionTimer += Time.deltaTime;
+        
+            if(exhaustionTimer >= exhaustionLength) 
+            {
+                return TaskStatus.Success;
+            }
+            return TaskStatus.Running;
+        }
     }
 }
